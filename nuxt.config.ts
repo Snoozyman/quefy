da5d3ec72324b99114268a26c6d56e84992acae5
Mikko Tuomikoski <me@snoozyman.eu>
2026-07-03 12:07:33 +0300

    chore: add prettier and eslint-config-prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -66,10 +66,7 @@ export default defineNuxtConfig({
 
   eslint: {
     config: {
-      stylistic: {
-        commaDangle: "never",
-        braceStyle: "1tbs",
-      },
+      stylistic: false,
     },
   },
 
